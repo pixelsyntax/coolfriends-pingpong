0b034e3d7d4bdcb857b73c4e4a8b87c4a6500fcc
pixelsyntax <tom@thomaspurnell.com>
2020-02-16 20:10:52 +1000

    Collect the loot

---
+++ coolfriends-raylib/loot.h
@@ -0,0 +1,11 @@
+#ifndef _LOOT_H
+#define _LOOT_H
+
+#include "raylib.h"
+
+typedef struct {
+	int x;
+	int y;
+} loot_t;
+
+#endif
--- coolfriends-raylib/main.c
+++ coolfriends-raylib/main.c
@@ -6,10 +6,16 @@
 
 #include "raylib.h"
 #include "player.h"
-#include "math.h"
+#include "loot.h"
+#include <math.h>
 
 int lerp(int a, int b, float d) {
     return roundf(a * (1 - d) + b * d);
+}
+
+void position_loot( loot_t* loot ) {
+    loot->x = GetRandomValue( -10, 10 );
+    loot->y = GetRandomValue( -10, 10 );
 }
 
 int main(void)
@@ -23,6 +29,8 @@ int main(void)
 
     Rectangle playerRect = { 400, 280, 40, 40 };
     player_t player = { 0, 0 };
+    loot_t loot;
+    position_loot( &loot );
 
     Camera2D camera = { 0 };
     camera.rotation = 0.0f;
@@ -62,6 +70,12 @@ int main(void)
             lerp(camera.target.x, playerRect.x, 0.1),
             lerp(camera.target.y, playerRect.y, 0.1) };
 
+        //Did the player get the loot?
+        while( loot.x == player.x && loot.y == player.y ){
+            position_loot( &loot );
+        }
+
+
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
@@ -84,14 +98,17 @@ int main(void)
                 
                 // draw the player
                 DrawRectangleRec(playerRect, RED);
+                // draw the loot
+                DrawRectangle( loot.x * playerRect.width, loot.y * playerRect.height, playerRect.width, playerRect.height, GOLD );
 
             EndMode2D();
 
             // show some UI text
-            DrawRectangle( 10, 10, 250, 60, Fade(SKYBLUE, 0.5f));
-            DrawRectangleLines( 10, 10, 250, 60, BLUE);
+            DrawRectangle( 10, 10, 250, 80, Fade(SKYBLUE, 0.5f));
+            DrawRectangleLines( 10, 10, 250, 80, BLUE);
             DrawText("First test for a grid based game", 20, 20, 10, BLACK);
             DrawText("- Move with cursor keys", 40, 40, 10, DARKGRAY);
+            DrawText("- Find the gold", 40, 60, 10, DARKGRAY);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
